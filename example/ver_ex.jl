368cf7fdf6729e6a87e892442df26e5f94d33712
Xiansheng Cai <iintsjds@gmail.com>
2021-05-18 16:08:36 -0400

    wip. ver_ex.jl still not working

--- example/ver_ex.jl
+++ example/ver_ex.jl
@@ -3,11 +3,29 @@
 
 using QuantumStatistics, LinearAlgebra, Random, Printf, BenchmarkTools, InteractiveUtils, Parameters
 # using ProfileView
+using PyCall
 
-const Steps = 1e7
+
+const Steps = 1e6
 
 include("parameter.jl")
 include("../application/electron_gas/RPA.jl")
+
+const n1=0
+const n2=0
+const ℓ=1
+
+special = pyimport("scipy.special")
+coeff = SVector{ℓ+1,Float64}(special.legendre(ℓ).c)
+exp = zeros(ℓ+1)
+for i=1:ℓ+1
+    exp[i]=ℓ+1-i
+end
+exp = SVector{ℓ+1,Float64}(exp)
+
+function legendre(x)
+    return dot((x .^ exp) ,coeff)
+end
 
 
 const qgrid = Grid.boseKUL(kF, 6kF, 0.000001*sqrt(me^2/β/kF^2), 15,4) 
@@ -32,10 +50,6 @@ function interaction(q, τIn, τOut)
     return -v / β, -w
 end
 
-function WRPA(τ,q)
-    return interaction(q,0,τ)[2]
-end
-
 # function WRPA(τ,q)
 #     # temporarily using a toy model
 #     g=1.0
@@ -53,40 +67,110 @@ end
 # end
 
 function integrand(config)
-    if config.curr != 1
-        error("impossible")
+    if config.curr == 1
+        T, K, Theta= config.var[1],config.var[2],config.var[3]
+        q = K[1]
+        t1 = T[1]
+        θ = Theta[1]
+
+        k1 = kF * @SVector[1, 0, 0]
+        k2 = kF * @SVector[cos(θ), sin(θ), 0]
+
+        ω1 = (dot(q-k1, q-k1) - kF^2) * β
+
+        ω2 = (dot(q-k2, q-k2) - kF^2) * β
+
+        τ3 = (t1)/β
+        g3 = Spectral.kernelFermiT(τ3, ω1)
+
+        τ6 = (-t1)/β
+        g6 = Spectral.kernelFermiT(τ6, ω2)
+
+        # W1 = WRPA(t, q-k1-k2)
+        # W2 = WRPA(t-t1-t2,q)
+
+        W1 = interaction(q-k1-k2, 0, 0/β)
+        W2 = interaction(q, (t1)/β,t1/β )
+
+        s_s = W1[1]*W2[1]*g3*g6*cos(π*(2*n1+1) * t1 /β) * cos(π*(2*n2+1) * t1/β)*β*β
+
+        factor = 1.0/(2π)^3 /2.0
+        return 0.0
+        return (s_s) * factor * sin(θ)*legendre(cos(θ))
+    elseif config.curr == 2
+        T, K, Theta= config.var[1],config.var[2],config.var[3]
+        q = K[1]
+        t1, t2 = T[1], T[2]
+        θ = Theta[1]
+
+        k1 = kF * @SVector[1, 0, 0]
+        k2 = kF * @SVector[cos(θ), sin(θ), 0]
+
+        ω1 = (dot(q-k1, q-k1) - kF^2) * β
+
+        ω2 = (dot(q-k2, q-k2) - kF^2) * β
+
+        τ3 = (t1)/β
+        g3 = Spectral.kernelFermiT(τ3, ω1)
+
+        τ4 = (-t2)/β
+        g4 = Spectral.kernelFermiT(τ4, ω2)
+
+        τ5 = (t2)/β
+        g5 = Spectral.kernelFermiT(τ5, ω1)
+
+        τ6 = (-t1)/β
+        g6 = Spectral.kernelFermiT(τ6, ω2)
+
+        # W1 = WRPA(t, q-k1-k2)
+        # W2 = WRPA(t-t1-t2,q)
+
+
+        W10 = interaction(q-k1-k2,0,(t1-t2)/β)
+        W20 = interaction(q,t2/β,t1/β)
+
+        s_r = W10[1]*W20[2]*g3*g4*cos(π*(2*n1+1) * t1 /β) * cos(π*(2*n2+1) * t2/β)
+        r_s = W10[2]*W20[1]*g5*g6*cos(π*(2*n1+1) * t1 /β) * cos(π*(2*n2+1) * t2/β)
+
+        factor = 1.0/(2π)^3 /2.0
+        return (s_r+r_s) * factor* sin(θ)*legendre(cos(θ))
+
+    elseif config.curr == 3
+        T, K, Theta= config.var[1],config.var[2],config.var[3]
+        q = K[1]
+        t, t1, t2 = T[1], T[2], T[3]
+        θ = Theta[1]
+
+        k1 = kF * @SVector[1, 0, 0]
+        k2 = kF * @SVector[cos(θ), sin(θ), 0]
+
+        ω1 = (dot(q-k1, q-k1) - kF^2) * β
+        τ1 = (t1-t)/β
+        g1 = Spectral.kernelFermiT(τ1, ω1)
+
+        ω2 = (dot(q-k2, q-k2) - kF^2) * β
+        τ2 = (β-t2-t)/β
+        g2 = -Spectral.kernelFermiT(τ2, ω2)
+
+        W1 = interaction(q-k1-k2, 0, t/β)
+        W2 = interaction(q, (t+t2)/β,t1/β )
+
+        r_r = W1[2]*W2[2]*g1*g2*cos(π*(2*n1+1) * t1 /β) * cos(π*(2*n2+1) * t2/β)/β/β
+
+        factor = 1.0/(2π)^3 /2.0
+        return (r_r) * factor* sin(θ)*legendre(cos(θ))
+
+    else
+        return 0.0
     end
 
-    T, K, Theta = config.var[1], config.var[2], config.var[3]
-    q = K[1]
-    t, t1, t2 = T[1], T[2], T[3]
-    θ = Theta[1]
-
-    k1 = kF * @SVector[1, 0, 0]
-    k2 = kF * @SVector[cos(θ), sin(θ), 0]
-
-    ω1 = (dot(q-k1, q-k1) - kF^2) * β
-    τ1 = (t1-t)/β
-    g1 = Spectral.kernelFermiT(τ1, ω1)
-
-    ω2 = (dot(q-k2, q-k2) - kF^2) * β
-    τ2 = (β-t1-t)/β
-    g2 = Spectral.kernelFermiT(τ2, ω2)
-
-    W1 = WRPA(t, q-k1-k2)
-    W2 = WRPA(t-t1-t2,q)
-
-    legendre=1.0
-    factor = 1.0
-
-    return g1 * g2 * W1 * W2 * factor * legendre * cos(π * t1 /β) * cos(π * t2/β)
 end
 
 function measure(config)
     obs = config.observable
     factor = 1.0 / config.reweight[config.curr]
     weight = integrand(config)
-    obs[1] += weight / abs(weight) * factor
+    obs[config.curr] += weight / abs(weight) * factor
 end
 
 function run(steps)
@@ -95,16 +179,28 @@ function run(steps)
     K = MonteCarlo.FermiK(3, kF, 0.2 * kF, 10.0 * kF)
     Angle = MonteCarlo.Angle()
 
-    dof = [[3, 1, 1],] # degrees of freedom of the normalization diagram and the bubble
-    obs = zeros(Float64,1) # observable forp the normalization diagram and the bubble
+    dof = [[1,1,1],[2,1,1],[3,1,1]] # degrees of freedom of the normalization diagram and the bubble
+    obs = zeros(Float64,3) # observable forp the normalization diagram and the bubble
+    nei = [[2,4],[3,1],[2,4],[1,3]]
 
-    config = MonteCarlo.Configuration(steps, (T, K, Angle), dof, obs)
+#    config = MonteCarlo.Configuration(steps, (T,K,Angle ), dof, obs)
+    config = MonteCarlo.Configuration(steps, (T,K,Angle ), dof, obs; neighbor = nei)
     avg, std = MonteCarlo.sample(config, integrand, measure; print=0, Nblock=16)
     # @profview MonteCarlo.sample(config, integrand, measure; print=0, Nblock=1)
     # sleep(100)
 
+    NF = -TwoPoint.LindhardΩnFiniteTemperature(dim, 0.0, 0, kF, β, me, spin)[1]
+    println("NF=$NF")
+
     if isnothing(avg) == false
-            @printf("%10.6f ± %10.6f\n", avg[1], std[1])
+        # @printf("%10.6e ± %10.6e\n", avg[1], std[1])
+        # @printf("%10.6e ± %10.6e\n", avg[2], std[2])
+        # @printf("%10.6e ± %10.6e\n", avg[3], std[3])
+        # @printf("%10.6e ± %10.6e\n", sum(avg), sqrt(sum(std .^ 2)))
+        @printf("%10.6f ± %10.6f\n", avg[1], std[1])
+        @printf("%10.6f ± %10.6f\n", avg[2], std[2])
+        @printf("%10.6f ± %10.6f\n", avg[3], std[3])
+        @printf("%10.6f ± %10.6f\n", sum(avg), sqrt(sum(std .^ 2)))
     end
 end
 
