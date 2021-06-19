b1e34cc819dab6fd3606d4fb41b9dfd750937fd1
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-06-18 22:24:44 -0400

    add eigen.jl

---
+++ application/superconductivity/eigen.jl
@@ -0,0 +1,79 @@
+using StaticArrays:similar
+using QuantumStatistics
+include("parameter.jl")
+
+"""
+calcF(Δ, kgrid, fdlr)
+
+calculate the F function in τ-k representation 
+
+# Arguments:
+
+- Δ: gap function in (ωn, k) representation
+- kgrid: momentum grid
+- fdlr::DLRGrid: DLR Grid that contains the imaginary-time grid
+"""
+function calcF(Δ, fdlr, kgrid)
+    ngrid = fdlr.n
+    Δ = DLR.tau2matfreq(:fermi, Δ, fdlr, fdlr.n, axis=1)
+    F = similar(Δ)
+    for (ki, k) in enumerate(kgrid)
+        ω = k^2 / (2me) - EF
+        for (ni, n) in enumerate(ngrid)
+            np = n # matsubara freqeuncy index for the upper G: (2np+1)π/β
+            nn = -n - 1 # matsubara freqeuncy for the upper G: (2nn+1)π/β = -(2np+1)π/β
+            F[ni, ki] = Δ[ni, ki] * Spectral.kernelFermiΩ(nn, ω, β) * Spectral.kernelFermiΩ(np, ω, β)
+    end
+end
+    F = DLR.matfreq2tau(:fermi, F, fdlr, fdlr.τ, axis=1)
+    return real(F)
+end
+
+function dH1(k, p, τ)
+    g = 1.0
+    gh = sqrt(g)
+    if abs(k - p) > 1.0e-12
+        return -2π * gh^3 * log((abs(k - p) + g) / abs(k - p)) * (exp(-gh * τ) + exp(-gh * (β - τ))) / (1 - exp(-gh * β))
+    else
+        return 0.0
+end
+end
+
+function bare(k, p)
+    if abs(k - p) > 1.0e-12
+        return 4π * e0^2 * log((k + p) / abs(k - p))
+    else
+        return 0.0
+end
+end
+
+function calcΔ(F, fdlr, kgrid, wkgrid)
+    Δ = similar(F)
+    Δ0 = zeros(Float64, length(kgrid))
+    Δ = zeros(Float64, (fdlr.size, length(kgrid)))
+    for (ki, k) in enumerate(kgrid)
+        for (qi, q) in enumerate(kgrid)
+            Δ0[ki] += bare(k, q) * F[1, qi] * wkgrid[qi]  # TODO: check -F(0^-, q)==F(0^+, q)
+            for (τi, τ) in enumerate(fdlr.τ)
+                # Δ[τi, ki] += dW[τi, qi, ki] * F[τi, qi] * wkgrid[qi]
+                Δ[τi, ki] += dH1(k, q, τ) * F[τi, qi] * wkgrid[qi]
+            end
+        end
+    end
+    return Δ 
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    fdlr = DLR.DLRGrid(:fermi, 10EF, β, 1e-10) 
+
+    MaxK = 5.0 * kF
+    Nk = 256
+    dK = MaxK / Nk
+    kgrid = LinRange(0.0, MaxK, Nk) .+ dK / 2.0
+    wkgrid = [Nk / MaxK for i in 1:Nk]
+    # Δ = zeros(ComplexF64, (fdlr.size, Nk)) .+ 1.0
+    Δ = zeros(Float64, (fdlr.size, Nk)) .+ 1.0
+    F = calcF(Δ, fdlr, kgrid)
+    Δ = calcΔ(F, fdlr, kgrid, wkgrid)
+    println(Δ[1, :])
+end
